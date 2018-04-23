e622f4cab6f12600baa614213db1ae1841f97cc0
cindych419@gmail.com <cindych419@gmail.com>
2018-04-23 03:37:01 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 def position_taken?(board, index)
 if board[index] == " "
   false
-if board[index] == ""
+elsif board[index] == ""
   false
-if board[index] == nil
+elsif board[index] == nil
   false
-if board[index] == "X" || "O"
+elsif board[index] == "X" || "O"
   true
 end
 end
